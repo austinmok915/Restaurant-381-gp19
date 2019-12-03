2fc2bd3a211caf4ecbde0349ef983c14f9a3d778
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-04 02:21:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -94,7 +94,8 @@ app.get('/list',(req, res) => {
 			const db = client.db(dbName);
 			const findRestaurant = (db, callback) => { 
 				let cursor2 = db.collection('restaurants').find()
-				res.status(200).render('restaurantList',{rname:JSON.stringify(cursor2)});
+				const namelist = JSON.stringify(cursor2);
+				res.status(200).render('restaurantList',{rname:namelist)};
 				callback();
 			}
 			client.connect((err) => { 
