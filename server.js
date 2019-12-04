653282258f17703c461214115a683fd40a4fc2b3
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-05 01:13:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/login', setCurrentTimestamp, (req, res) => {
 			const db = client.db(dbName);
 			const findUser = (db, callback) => {
 				let cursor = db.collection('user').find() ;
-				cursor.every((account) => { 
+				cursor.forEach((account) => { 
 					
 				if (account.name == req.body.name && account.password == req.body.password) {
 					return false	}
